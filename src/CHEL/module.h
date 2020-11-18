672ddea1404883e1a5b27c4518754d7b0ba4eea1
Riku <riku@kawaii.sh>
2020-11-18 00:57:34 -0600

    Replaced webserver and removed boost dependency

--- src/CHEL/module.h
+++ src/CHEL/module.h
@@ -6,9 +6,6 @@
 #define METHOD_BIND(T, F) [](JsValueRef call, bool isConstructCall, JsValueRef *args, unsigned short argumentCount, void *callbackState) -> void* { \
   return std::invoke((F), static_cast<std::add_pointer_t<T>>(callbackState), call, isConstructCall, args, argumentCount); }
 
-// Only use this in functions
-#define CHECK(cmd) if (cmd != JsNoErr) { return JS_INVALID_REFERENCE; }
-
 // Abstract class for modules
 namespace JS {
     class Module {
